42592c582bebe0da428f6657ad9dce7df778d00f
arehman1806 <arehmannasir@outlook.com>
2022-03-15 04:05:58 +0000

    eve renames buffer file

--- eve.py
+++ eve.py
@@ -5,11 +5,12 @@ from common import *
 from const import *
 
 dialog = Dialog('print')
-while(True):
-    f = open(BUFFER_DIR+BUFFER_FILE_NAME, "r")
-    print(f.read())
-    f.close()
-    time.sleep(0.1)
+# while(True):
+#     f = open(BUFFER_DIR+BUFFER_FILE_NAME, "r")
+#     print(f.read())
+#     f.close()
+#     time.sleep(0.1)
+os.rename('/tmp/s1829279/buffer', '/tmp/s1829279/buffer1')
 # player = os.path.basename(sys.argv[0]).split('.', 1)[0]
 # print(player)
 # socket, aes = setup(player, BUFFER_DIR, BUFFER_FILE_NAME)

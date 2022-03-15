13d9aff89a0dc16ff056a0f6c631bc8a17e12f9a
arehman1806 <arehmannasir@outlook.com>
2022-03-15 04:01:24 +0000

    Eve reads buffer file

--- eve.py
+++ eve.py
@@ -1,9 +1,16 @@
 import sys
 import os
+import time
 from common import *
 from const import *
 
 dialog = Dialog('print')
-player = os.path.basename(sys.argv[0]).split('.', 1)[0]
-print(player)
-socket, aes = setup(player, BUFFER_DIR, BUFFER_FILE_NAME)
+while(True):
+    f = open(BUFFER_DIR+BUFFER_FILE_NAME, "r")
+    print(f.read())
+    f.close()
+    time.sleep(0.1)
+# player = os.path.basename(sys.argv[0]).split('.', 1)[0]
+# print(player)
+# socket, aes = setup(player, BUFFER_DIR, BUFFER_FILE_NAME)
+# os.rename()

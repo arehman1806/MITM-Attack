8877fde8ba045a72d1aca3bab1d44711e6dd0155
arehman1806 <arehmannasir@outlook.com>
2022-03-15 19:46:36 +0000

    printing flag

--- eve.py
+++ eve.py
@@ -6,11 +6,9 @@ from const import *
 
 
 dialog = Dialog('print')
-# while(True):
-#     f = open(BUFFER_DIR+BUFFER_FILE_NAME, "r")
-#     print(f.read())
-#     f.close()
-#     time.sleep(0.1)
+
+flag = sys.argv[0]
+print(flag)
 
 os.rename('/tmp/s1829279/buffer', '/tmp/s1829279/buffer1')
 
@@ -24,9 +22,8 @@ player_a = 'alice'
 socket_a, aes_a = setup(player_a, BUFFER_DIR, BUFFER_FILE_NAME)
 
 received = receive_and_decrypt(aes_a, socket_a)
-print('bob said: ' + received)
-
 heart_break = 'I hate you!'
+print('bob said: {}. But i will say: {}. HEHEHEHEHE'.format(received, heart_break))
 encrypt_and_send(heart_break, aes_b, socket_b)
 
 received_from_alice = receive_and_decrypt(aes_b, socket_b)

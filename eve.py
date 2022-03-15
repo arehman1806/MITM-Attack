efb518e83b1fb199cad094d03f87f4948831384b
arehman1806 <arehmannasir@outlook.com>
2022-03-15 04:19:00 +0000

    eve poses as alice

--- eve.py
+++ eve.py
@@ -11,7 +11,8 @@ dialog = Dialog('print')
 #     f.close()
 #     time.sleep(0.1)
 os.rename('/tmp/s1829279/buffer', '/tmp/s1829279/buffer1')
-# player = os.path.basename(sys.argv[0]).split('.', 1)[0]
-# print(player)
-# socket, aes = setup(player, BUFFER_DIR, BUFFER_FILE_NAME)
-# os.rename()
+player = 'alice'
+print(player)
+socket, aes = setup(player, BUFFER_DIR, BUFFER_FILE_NAME)
+received = receive_and_decrypt(aes, socket)
+print(received)
